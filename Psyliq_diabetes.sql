9d401786b7f67fd98518a6c31093e2668473f08b
Vaishnavi Chandrakant Badgujar <121229180+Vaishnavi-720@users.noreply.github.com>
2024-03-27 23:06:12 +0530

    Add files via upload

---
+++ Psyliq_diabetes.sql
@@ -0,0 +1,8 @@
+create database Psyliq;
+use Psyliq;
+select * from diabetes_prediction;
+select count(*) from diabetes_prediction;
+drop table diabetes_prediction;
+
+select * from diabetesp;
+select count(*) from diabetesp;
